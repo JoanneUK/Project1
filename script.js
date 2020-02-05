3b3731ff9a10fe1bc6821bc523050a00f61918b2
Joanne Nicholson <joanne.s.nicholson@gmail.com>
2020-02-05 14:12:51 -0800

    added file

---
+++ script.js
@@ -0,0 +1,22 @@
+// https://cors-anywhere.herokuapp.com
+let apiKey = "buYPH6YQCgvFYf4-xQrpKutnoUroVMOrUiAg_W2ORu34RqyAEAFen-T8tbUnV-sRCsbUc6RliSPlQ7sg_unY67RcLG5Bw2QwmjKrFP2uLZdVe_5j-TmcmHZ9JBE7XnYx"
+let term = 'chipotle';
+let loc = 'Los Angeles';
+let queryURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${loc}`;
+
+$.ajax({
+    url: queryURL,
+    method: "GET",
+    headers: {
+        Authorization: `Bearer ${apiKey}`
+    }
+  }).then(function(response) {
+debugger;
+    console.log(response);
+  }).catch(error => {
+      debugger;
+  });
+
+
+
+  
